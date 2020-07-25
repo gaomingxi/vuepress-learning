2ca9d20105ef00816654f4b1c91bb14b76e59c66
gaomx <gaomx365@163.com>
2020-07-25 15:52:34 +0800

    03-处理导航栏展示问题

---
+++ 03-处理导航栏展示问题/vuepress-starter/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  title: 'VuePress 中文文档 | VuePress 中文网', // 网站标题
+  description: 'Just playing around', // 网站描述
+  // 导航栏
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/guide/' },
+      { text: 'External', link: 'https://google.com' },
+    ]
+  }
+}
